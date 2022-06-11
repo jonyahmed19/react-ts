41a419ae824a5610f6e974fe3f5452800d8397af
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-11 09:13:32 +0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let name: [string, number];
+
+  name = ["Sahidul", 27];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Hello TypeScript</h2>
+        <h6>
+          I am {name[0]}, age is {name[1]}
+        </h6>
       </header>
     </div>
   );
