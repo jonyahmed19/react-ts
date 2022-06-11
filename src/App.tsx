1dd506525531c93e6b2f6047f15d67b02b8a559b
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-11 11:13:24 +0600

    starts the todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import InputField from "./InputField";
 
 function App() {
   let name: [string, number];
 
   name = ["Sahidul", 27];
+  type X = {
+    a: string;
+    b: number;
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Hello TypeScript</h2>
-        <h6>
-          I am {name[0]}, age is {name[1]}
-        </h6>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
 }
