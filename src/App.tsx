934fae2ff66c7fd8055582320a03c675ab2985d5
Sahidul Islam <jonyahmed19@gmail.com>
2022-06-11 11:23:06 +0600

    css done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import "./App.css";
-import InputField from "./InputField";
+import InputField from "./components/InputField";
 
 function App() {
-  let name: [string, number];
-
-  name = ["Sahidul", 27];
-  type X = {
-    a: string;
-    b: number;
-  };
-
   return (
     <div className="App">
       <span className="heading">Taskify</span>
